5360e0114bbadbfed48e19add453755e486c9a18
Heyzi <planol@me.com>
2015-05-29 20:36:59 +0300

    111

--- v0/js/google-map.js
+++ v0/js/google-map.js
@@ -1,9 +1,16 @@
 function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:8,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+  var myLatlng = new google.maps.LatLng(59.575129, 30.121597);
+  var mapOptions = {
+    zoom: 7,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
