c5e52fde9b3bbbd969057bc694a4546dc071d129
Heyzi <planol@me.com>
2015-05-29 19:20:26 +0300

    111

--- v0/js/map.js
+++ v0/js/map.js
@@ -1,23 +1,9 @@
-$(function () {
-    $("#contactf").click(function () {
-        $("#dialog").dialog({
-            modal: true,
-            title: "Google Map",
-            width: 600,
-            hright: 450,
-            buttons: {
-                Close: function () {
-                    $(this).dialog('close');
-                }
-            },
-            open: function () {
-                var mapOptions = {
-                    center: new google.maps.LatLng(19.0606917, 72.83624970000005),
-                    zoom: 18,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-                var map = new google.maps.Map($("#dvMap")[0], mapOptions);
-            }
-        });
-    });
+$(document).ready(function(){
+    PopUpHide();
 });
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
