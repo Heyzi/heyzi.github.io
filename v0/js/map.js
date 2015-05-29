ed660a45a9bcd2c43623b87c06d068cf84b181d6
Heyzi <planol@me.com>
2015-05-29 18:26:59 +0300

    11

--- v0/js/map.js
+++ v0/js/map.js
@@ -1,10 +1,23 @@
-function popupOpen(){
-    document.getElementById("popup").style.display="block";
-    document.getElementById("overlay").style.display="block";
-}
- 
-// Popup Close
-function popupClose(){
-    document.getElementById("popup").style.display="none";
-    document.getElementById("overlay").style.display="none";
-}
+$(function () {
+    $("#btnShow").click(function () {
+        $("#dialog").dialog({
+            modal: true,
+            title: "Google Map",
+            width: 600,
+            hright: 450,
+            buttons: {
+                Close: function () {
+                    $(this).dialog('close');
+                }
+            },
+            open: function () {
+                var mapOptions = {
+                    center: new google.maps.LatLng(19.0606917, 72.83624970000005),
+                    zoom: 18,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                var map = new google.maps.Map($("#dvMap")[0], mapOptions);
+            }
+        });
+    });
+});
