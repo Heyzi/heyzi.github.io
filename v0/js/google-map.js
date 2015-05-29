4a480487be5ba171e7a4edde7b55decace5d278b
Heyzi <planol@me.com>
2015-05-29 21:28:46 +0300

    444

--- v0/js/google-map.js
+++ v0/js/google-map.js
@@ -13,7 +13,7 @@ function initialize() {
       mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   
-  map=new google.maps.Map(document.getElementById("map_canvas"),mapProp);
+  map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
   marker.setMap(map);
     
   google.maps.event.addListener(marker, 'click', function() {
@@ -27,7 +27,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 google.maps.event.addDomListener(window, "resize", resizingMap());
 
-$('#map_canvas').on('show.bs.modal', function() {
+$('#myMapModal').on('show.bs.modal', function() {
    //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
    resizeMap();
 })
