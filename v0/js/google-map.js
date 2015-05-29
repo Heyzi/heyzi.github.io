477d97901c8ea72fd7935c3fe8d8d90e8a27bb39
Heyzi <planol@me.com>
2015-05-29 21:38:20 +0300

    1111

--- v0/js/google-map.js
+++ v0/js/google-map.js
@@ -1,45 +1,18 @@
-var map;        
-            var myCenter=new google.maps.LatLng(53, -1.33);
-var marker=new google.maps.Marker({
-    position:myCenter
-});
-
 function initialize() {
-  var mapProp = {
-      center:myCenter,
-      zoom: 14,
-      draggable: false,
-      scrollwheel: false,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  
-  map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
-  marker.setMap(map);
-    
-  google.maps.event.addListener(marker, 'click', function() {
-      
-    infowindow.setContent(contentString);
-    infowindow.open(map, marker);
-    
-  }); 
-};
+  var myLatlng = new google.maps.LatLng(59.575129, 30.121597);
+  var mapOptions = {
+    zoom: 7,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+  google.maps.event.trigger(map, "resize");
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
-
-google.maps.event.addDomListener(window, "resize", resizingMap());
-
-$('#myMapModal').on('show.bs.modal', function() {
-   //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
-   resizeMap();
-})
-
-function resizeMap() {
-   if(typeof map =="undefined") return;
-   setTimeout( function(){resizingMap();} , 400);
-}
-
-function resizingMap() {
-   if(typeof map =="undefined") return;
-   var center = map.getCenter();
-   google.maps.event.trigger(map, "resize");
-   map.setCenter(center); 
-}
+google.maps.event.trigger(map, "resize");
