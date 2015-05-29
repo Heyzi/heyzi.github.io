e334147a8db084813c07ef1f8e9cd28f18e3e395
Heyzi <planol@me.com>
2015-05-29 20:27:04 +0300

    66
    66

--- v0/js/google-map.js
+++ v0/js/google-map.js
@@ -1,7 +1,7 @@
 function initialize() {
   var mapProp = {
     center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
+    zoom:0,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
