219b2ec452a17529d59f0011149c9baabb700ebb
Heyzi <planol@me.com>
2015-05-29 23:26:31 +0300

    555

---
+++ v0/js/custom.js
@@ -0,0 +1,6 @@
+$('a[rel=popover]').popover({
+  html: true,
+  trigger: 'hover',
+  placement: 'right',
+  content: function(){return '<img src="'+$(this).data('img') + '" />';}
+});
--- v2/js/google-map.js
+++ v2/js/google-map.js
@@ -1,16 +1,1 @@
-function initialize() {
-  var myLatlng = new google.maps.LatLng(59.575129, 30.121597);
-  var mapOptions = {
-    zoom: 7,
-    center: myLatlng
-  }
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+$("[data-toggle=popover]").popover();
