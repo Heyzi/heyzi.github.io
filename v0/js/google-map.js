3a4440ea045a58884b931c8a4acc02dfae501474
Heyzi <planol@me.com>
2015-05-29 22:24:48 +0300

    333

--- v0/js/google-map.js
+++ v0/js/google-map.js
@@ -1,13 +1,23 @@
-function showMap() {
-        var mapOptions = {
-            zoom: 12,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-        $('#myModal').on('shown.bs.modal', function () {
-              google.maps.event.trigger(map, 'resize');
-              map.setCenter(new google.maps.LatLng(54, -2));
-            });
-        $('#myModal').modal("show");
+$(function() {
+  var $modal = $('#mymodal'),
+    $map = $('#map_canvas');
+  $modal.on('shown.bs.modal', function () {
+    google.maps.event.trigger($map[0], 'resize');
+  });
+});
+function initialize() {
+  var myLatlng = new google.maps.LatLng(59.575129, 30.121597);
+  var mapOptions = {
+    zoom: 7,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
