9bb91718998877e95c61c2fdb2ea277fd057690c
Heyzi <planol@me.com>
2015-05-30 11:43:31 +0300

    111

--- v0/js/map.js
+++ v0/js/map.js
@@ -1,33 +1,14 @@
-var var_map;
-var var_location = new google.maps.LatLng(45.430817,12.331516);
-	
-function map_init() {		 	
-			
-            var var_mapoptions = {
-              center: var_location,
-              zoom: 18,
-              mapTypeId: google.maps.MapTypeId.ROADMAP,
-              mapTypeControl: false,
-              panControl:false,
-              rotateControl:false,
-              streetViewControl: false,
-            };
-			
-				var_map = new google.maps.Map(document.getElementById("map-container"),
-						var_mapoptions);
-	
-			 
-				  google.maps.event.addListener(var_marker, 'click', function(e) {
-					 var_infobox.open(var_map, this);
-				  });
-		  
-			var var_infobox = new InfoBox(var_infobox_props);
-	
-				var_infobox.open(var_map, var_marker);
-			
-      }
-		  
-				google.maps.event.addDomListener(window, 'load', map_init);
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("map-container"),mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
       
       //start of modal google map
       $('#mapmodals').on('shown.bs.modal', function () {
